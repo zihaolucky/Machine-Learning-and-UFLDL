541d7694c6f377098672f90832a5718240cdbe1b
Zihao Zheng <zihaolucky@gmail.com>
2014-03-13 00:22:21 +0800

    #1 Sparse encoder

---
+++ UFLDL/Exercise/starter/computeNumericalGradient.m
@@ -0,0 +1,37 @@
+function numgrad = computeNumericalGradient(J, theta)
+% numgrad = computeNumericalGradient(J, theta)
+% theta: a vector of parameters
+% J: a function that outputs a real-number. Calling y = J(theta) will return the
+% function value at theta. 
+  
+% Initialize numgrad with zeros
+numgrad = zeros(size(theta));
+
+%% ---------- YOUR CODE HERE --------------------------------------
+% Instructions: 
+% Implement numerical gradient checking, and return the result in numgrad.  
+% (See Section 2.3 of the lecture notes.)
+% You should write code so that numgrad(i) is (the numerical approximation to) the 
+% partial derivative of J with respect to the i-th input argument, evaluated at theta.  
+% I.e., numgrad(i) should be the (approximately) the partial derivative of J with 
+% respect to theta(i).
+%                
+% Hint: You will probably want to compute the elements of numgrad one at a time. 
+
+epsilon = 10^-4;
+for i = 1:size(theta,1)
+    fprintf('*');
+    if(mod(i,50)==0)
+        fprintf('%2.2f\n',i/size(theta,1));
+    end
+    theta1 = theta;
+    theta2 = theta;
+    theta1(i) = theta(i) + epsilon;
+    theta2(i) = theta(i) - epsilon;
+    numgrad(i) = (J(theta1) - J(theta2)) / (2*epsilon);
+end
+
+
+
+%% ---------------------------------------------------------------
+end
